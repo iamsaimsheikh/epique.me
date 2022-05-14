45b4d0089a0862549081f13fcf0ad24748277308
saim <iamsaimsheikh@gmail.com>
2022-05-14 14:37:36 +0500

    Add content to sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,7 +3,10 @@ import Styles from './sidebar.module.scss'
 
 const sidebar = () => {
   return (
-    <div className={Styles.sidebarContainer}>sidebar</div>
+    <div className={Styles.sidebarContainer}>
+      <h4>Who are you looking for?</h4>
+      <input type='text' />
+    </div>
   )
 }
 
